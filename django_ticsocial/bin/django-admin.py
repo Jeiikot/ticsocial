72a9d544da04cebb1aa467d72132633b71528138
Jeiikot <jeiikot21@gmail.com>
2020-11-30 15:52:22 -0500

    [CREATE] Django project. - [ADD] gitignore, requirements.tx and persona APP.

---
+++ django_ticsocial/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/tesla/Documentos/Pruebas/ticsocial/django_ticsocial/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
